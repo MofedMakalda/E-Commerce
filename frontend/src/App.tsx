5bfc779883e33a64b76f29310fc49c671c6425cb
mofedmakalda <mofedmk1@gmail.com>
2024-08-14 22:42:51 +0300

    mui installation + navbar top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path= "/" element={<HomePage />} />
       </Routes>
